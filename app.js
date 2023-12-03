e25f57556add7cc8efa2e9e975ff57b0c45ef79e
Kijun Song <skjy1998@hansung.ac.kr>
2023-12-04 00:18:22 +0900

    update app.js

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const http = require('http').Server(app);
+const http = require("http").Server(app);
 
 const port = process.env.PORT || 3000;
 
-// 루트(/) 경로로 들어오는 HTTP GET 요청을 처리한다. 보라색으로 표현된 코드가 핸들러 
-app.get('/', (req, res) => {
+// 루트(/) 경로로 들어오는 HTTP GET 요청을 처리한다. 보라색으로 표현된 코드가 핸들러
+app.get("/", (req, res) => {
   // 루트 경로에서 쇼핑몰 홈페이지를 직접 렌더링
   res.send(`
     <!DOCTYPE html>
@@ -23,7 +23,7 @@ app.get('/', (req, res) => {
     </body>
     </html>
   `);
-} );
+});
 
 http.listen(port, () => {
   console.log(`Server is running on port ${port}`);
